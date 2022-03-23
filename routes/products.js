7330e1c8e05ff7b147bc3ceea2be388edd243f38
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-03-23 00:36:35 -0600

    Se empezo a utilizar los servicios

--- routes/products.js
+++ routes/products.js
@@ -1,23 +1,13 @@
+
 const express = require('express');
-const faker = require('faker')
+const ProductsService = require('./../services/products')
 
 const router = express.Router();
+const service = new ProductsService();
 
 
-router.get('/', (req, res)=>{
-    const products = []
-    const { size } = req.query;
-
-    const limit = size || 10;
-
-    for (let i = 0; i < limit; i++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(),10),
-            image: faker.image.imageUrl()
-        })
-    }
-
+router.get('/', (req, res)=>{    
+    const products = service.find()
     res.json(products)
 })
 
@@ -30,19 +20,9 @@ router.get('/filter', (req, res) =>{
 
 router.get('/:id', (req, res) =>{
     const {id} = req.params;
-    if (id === '999') {
-        res.status(404).json({
-            message: "not found"            
-        });        
-    }else{
-        res.status(200).json({
-            id,
-            name: "Product 2",
-            price: 2000
-        });
-    }
-
+    const product = service.findOne(id);
     
+    res.status(200).json(product)
 })
 
 router.post('/', (req, res)=>{

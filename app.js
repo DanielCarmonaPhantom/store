bd195902d7841f6b3ab376c2f53c1d6fa7c9bb3d
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-03-24 11:26:40 -0600

    Habilitamos el cors y la whitelist

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const routerApi = require('./routes')
 const cors = require('cors')
+
+const routerApi = require('./routes')
+
 
 const { logErrors, errorHandler , boomErrorHandler} = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
+
+const whitelist = ['http://localhost:5500'];
+const options ={
+    origin: (origin, callback)=>{
+        if (whitelist.includes(origin)) {
+            callback(null, true);
+        }else{
+            callback(new Error('no permitido'));
+        }
+    }
+}
 
 app.use(cors())
 app.use(express.json());
